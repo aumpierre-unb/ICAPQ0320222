303d801710a62fa15e201f2012e8e7d8cf69095c
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-10-13 09:32:31 -0300

    Initial commit

---
+++ template.jl
@@ -0,0 +1,45 @@
+#Universidade de Brasília
+#ICAPQ – IQD0003
+#Turma T01                       # Ajuste se necessário!!
+#Prova P1 - 2022/2 – 13/12/2022  # Ajuste se necessário!!
+#Prof. Alexandre Umpierre
+
+#Nome: xxx
+#Matrícula: xxx
+
+#=
+Instruções:
+1)  Leia atentamente os enunciados.
+2)  Não é necessário copiar os enunciados nas respostas.
+3)  A avaliação deve ser realizada exclusivamente
+    nos computadores da sala de aula
+    e utilizando a linguagem Julia na IDE VS Code.
+4)  É vedado o uso de telefone celular e de internet,
+    para qualquer finalidade, durante a avaliação.
+5)  É vedada a consulta a colegas e monitores
+    e a qualquer material além do Help offline de Julia
+    e do seu acervo pessoal de rotinas e funções.
+6)  Cada questão deve ser entregue em
+    um e um único arquivo.
+7)  Cada arquivo deve ser nomeado segundo os exemplos:
+    ICAPQ_T01_20220913_P2_Q1_suamatricula.jl
+    ICAPQ_T01_20220913_P2_Q1_110049081.jl
+8)  Arquivos entregues sem extensão ou
+    com extensão diferente de .jl
+    não serão corrigidos.
+9)  Arquivos mal identificados não serão corrigidos.
+10) Altere a data e os números da prova e da questão,
+    se necessário.
+11) As questões devem acompanhar, obrigatoriamente,
+    a sequência de comandos, na forma de script,
+    para atender ao enunciado.
+    Nenhuma ação do professor deve ser requerida para
+    a execução do arquivo.
+12) Apague as ocorrências xxx e preencha com seus dados:
+    nome, matrícula, número e resolução da questão.
+13) Este bloco de comentários deve ser apagado para
+    entrega do arquivo.
+=#
+
+#Questão xxx:
+#Apresente a resolução aqui
